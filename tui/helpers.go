ced2b2906051c9484f95d8e38a3e71e6259150cd
Miraç Bedir <mirac.bedir.tr@gmail.com>
2026-07-14 11:45:16 +0300

    feat: if data is an image it is denoted by image->

--- tui/helpers.go
+++ tui/helpers.go
@@ -1,6 +1,9 @@
 package tui
 
 func maxCharOfString(s string) string {
+	if len(s) <= 25 {
+		return s
+	}
 	runes := []rune(s)
 	return string(runes[:25]) + "..."
 }
--- tui/tui.go
+++ tui/tui.go
@@ -1,10 +1,12 @@
 package tui
 
 import (
+	"bytes"
 	"fmt"
+	"net/http"
 
 	tea "charm.land/bubbletea/v2"
-	"github.com/atotto/clipboard"
+
 	"github.com/bedirmirac/glipboard/storage"
 )
 
@@ -43,52 +45,121 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if m.cursor < len(m.choices)-1 {
 				m.cursor++
 			}
+		case "right", "l":
+			if m.cursor+10 < len(m.choices)-1 {
+				m.cursor += 10
+			} else {
+				m.cursor = len(m.choices) - 1
+			}
+		case "left", "h":
+			if m.cursor-10 > 0 {
+				m.cursor -= 10
+			} else {
+				m.cursor = 0
+			}
 		case "enter":
 			_, ok := m.selected[m.cursor]
 			if ok {
 				/*copy selected logic */
-				textToCopy := m.choices[m.cursor].Context
-				err := clipboard.WriteAll(textToCopy)
-				if err != nil {
-					m.message = fmt.Sprintf("The content couldn't be copied: %v", err)
-				}
-				m.message = "The content is copied."
-				return m, tea.Quit
-			} else {
-				if len(m.selected) > 0 {
-					m.message = "You can only select one item"
+				choice := m.choices[m.cursor]
+				var req *http.Request
+				var err error
+
+				if choice.Type == "text" {
+					req, err = http.NewRequest("POST", "http://127.0.0.1:49321/request?type=text", bytes.NewBufferString(choice.Context))
+				} else if choice.Type == "image" {
+					req, err = http.NewRequest("POST", "http://127.0.0.1:49321/request?type=image", bytes.NewBufferString(choice.FilePath))
 				} else {
-					m.selected[m.cursor] = struct{}{}
+					m.message = "Error: Unsupported data type."
+					return m, nil
 				}
+
+				if err != nil {
+					m.message = "Error: Could not create HTTP request."
+					return m, nil
+				}
+
+				/* Send the request to the background Daemon */
+				client := &http.Client{}
+				resp, err := client.Do(req)
+				if err != nil {
+					m.message = fmt.Sprintf("ERROR: Could not connect to daemon: %v", err)
+					return m, nil
+				}
+				defer resp.Body.Close()
+
+				if resp.StatusCode != http.StatusOK {
+					m.message = fmt.Sprintf("ERROR: Daemon returned status code %d", resp.StatusCode)
+					return m, nil
+				}
+
+				return m, tea.Quit
 			}
 		case "d":
 			_, ok := m.selected[m.cursor]
 			if ok {
 				/*delete function*/
-				idToDelete := m.choices[m.cursor].ID
-				err := m.db.Delete(idToDelete)
+				choice := m.choices[m.cursor]
+				var req, req1 *http.Request
+				var reqs []*http.Request
+				var err error
+				req, err = http.NewRequest("POST", "http://127.0.0.1:49321/request?type=delete", bytes.NewBufferString(choice.Hash))
+				if err == nil {
+					reqs = append(reqs, req)
+				}
+				if choice.Type == "image" {
+					req1, err = http.NewRequest("POST", "http://127.0.0.1:49321/request?type=deleteImagePath", bytes.NewBufferString(choice.FilePath))
+					if err == nil {
+						reqs = append(reqs, req1)
+					}
+				}
+
 				if err != nil {
-					m.message = fmt.Sprintf("The content couldn't be deleted: %v", err)
-					return m, tea.Quit
+					m.message = "Error: Could not create HTTP request."
+					return m, nil
 				}
-				m.choices = append(m.choices[:m.cursor], m.choices[m.cursor+1:]...)
-				m.message = "The content is deleted."
-				delete(m.selected, m.cursor)
-				m.cursor = 0
-				return m, nil
-			} else {
-				if len(m.selected) > 0 {
-					m.message = "You can only select one item"
-				} else {
-					m.selected[m.cursor] = struct{}{}
+
+				/* Send the request to the background Daemon */
+				for _, r := range reqs {
+					client := &http.Client{}
+					r, err := client.Do(r)
+					if err != nil {
+						m.message = fmt.Sprintf("ERROR: Could not connect to daemon: %v", err)
+						return m, nil
+					}
+					defer r.Body.Close()
+					if r.StatusCode != http.StatusOK {
+						m.message = fmt.Sprintf("ERROR: Daemon returned status code %d", r.StatusCode)
+						return m, nil
+					}
 				}
-			}
-		case "ctrl+r":
-			err := m.db.DeleteAll()
-			if err != nil {
-				m.message = fmt.Sprintf("The contents could't be deleted: %v", err)
+
 				return m, tea.Quit
 			}
+
+		case "ctrl+r":
+			var req *http.Request
+			var err error
+			req, err = http.NewRequest("POST", "http://127.0.0.1:49321/request?type=deleteAll", nil)
+			if err != nil {
+				m.message = "Error: Could not create HTTP request."
+				return m, nil
+			}
+
+			/* Send the request to the background Daemon */
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				m.message = fmt.Sprintf("ERROR: Could not connect to daemon: %v", err)
+				return m, nil
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				m.message = fmt.Sprintf("ERROR: Daemon returned status code %d", resp.StatusCode)
+				return m, nil
+			}
+
 			m.message = "All of the contents is deleted!"
 			return m, tea.Quit
 		case "space":
@@ -134,12 +205,12 @@ func (m model) View() tea.View {
 		if _, ok := m.selected[i]; ok {
 			checked = "x"
 		}
-		maxContext := choice.Context
-		if len(choice.Context) > 25 {
-			maxContext = maxCharOfString(choice.Context)
+		if choice.Context == "" {
+			choice.Context = "Image->" + choice.FilePath
 		}
 
-		s += fmt.Sprintf("\n%v [%v] %v\n", cursor, checked, maxContext)
+		choice.Context = maxCharOfString(choice.Context)
+		s += fmt.Sprintf("\n%v [%v] %v\n", cursor, checked, choice.Context)
 	}
 
 	if m.message != "" {
